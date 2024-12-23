cfb0d7a310ce7b535391ff31baa5f79ecc2fc9bb
TNT <139092950+TNTxTrick@users.noreply.github.com>
2024-12-23 12:34:05 +0700

    Update spam.js
    Signed-off-by: TNT <139092950+TNTxTrick@users.noreply.github.com>

--- plugins/commands/spam.js
+++ plugins/commands/spam.js
@@ -1,11 +1,1 @@
-module.exports = {
-    name: "spam",
-    author: "",
-    category: "SYSTEMS",
-    version: "1.0",
-    nopre: false,
-    admin: false,
-    wait: 3,
-    desc: "spam sms.",
-    async onCall({ message, args, user }) {
-        try {
+
